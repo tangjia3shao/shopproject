6ec0639572b7c0836ec519bcb4498334313449fe
tang <tang@itheima.com>
2018-09-19 18:19:17 +0800

    商家注册加密完成

--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,11 +1,27 @@
 package com.pinyougou.sellergoods.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.pinyougou.pojo.TbBrand;
+
+import entity.PageResult;
 
 public interface BrandService {
 	
 	public List<TbBrand> findAll();
-
+	
+	public PageResult findPage(int pageNum ,int pageSize);
+	
+	public void save(TbBrand brand);
+	public void update(TbBrand brand);
+	
+	public TbBrand findOne(Long id);
+	
+	public void deleat(Long[] ids);
+	
+	public PageResult findPage(TbBrand brand , int pageNum ,int pageSize);
+	
+	List<Map> selectOptionList();
+ 
 }
